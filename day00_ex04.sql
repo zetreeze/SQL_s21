11da63d813754fada78b42990eff8291846b28d5
Simon <jeanscucumber@gmail.com>
2024-11-20 23:16:47 +0300

    Delete day00_ex04.sql

--- day00_ex04.sql
+++
@@ -1,5 +0,0 @@
---SELECT * FROM person WHERE id = 1 
-
-SELECT CONCAT(name, ' (age:', age,',gender:''', gender, ''',address:''', address,''')') as column_info
-FROM person 
-ORDER BY column_info ASC
