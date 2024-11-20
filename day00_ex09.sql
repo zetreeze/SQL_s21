1242b5428bcee9dc7a7a59b92e25fbc692998d96
Simon <jeanscucumber@gmail.com>
2024-11-20 23:17:43 +0300

    Delete day00_ex09.sql

--- day00_ex09.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	(SELECT name FROM person WHERE person.id=pv.person_id) as person_name,
-	(SELECT name FROM pizzeria WHERE pizzeria.id=pv.pizzeria_id) as pizzeria_name
-FROM (SELECT person_id, pizzeria_id FROM person_visits WHERE visit_date BETWEEN '2022-01-07' and '2022-01-09') AS pv
-ORDER BY pizzeria_id DESC, person_name ASC
-
-
-
